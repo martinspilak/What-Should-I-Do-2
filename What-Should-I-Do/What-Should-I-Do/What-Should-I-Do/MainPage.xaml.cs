e9be4a865534905f225db3e93f9d04a19e07b3eb
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-02-02 18:08:00 +0100

    stacklayout

--- What-Should-I-Do/What-Should-I-Do/What-Should-I-Do/MainPage.xaml.cs
+++ What-Should-I-Do/What-Should-I-Do/What-Should-I-Do/MainPage.xaml.cs
@@ -8,8 +8,6 @@ using Xamarin.Forms;
 
 namespace What_Should_I_Do
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
