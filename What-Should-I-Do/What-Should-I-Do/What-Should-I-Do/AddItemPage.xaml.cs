0feeeb05995070251aa6cdde4bd311ee5dfe29ec
MartinNovysedlak <martinnovysedlak48@gmail.com>
2020-02-02 19:00:01 +0100

    new page

---
+++ What-Should-I-Do/What-Should-I-Do/What-Should-I-Do/AddItemPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace What_Should_I_Do
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class AddItemPage : ContentPage
+{
+    public AddItemPage()
+    {
+        InitializeComponent();
+    }
+}
+}
